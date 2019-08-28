b8a3fcff3efeaebe63bda004d3f376bd32f86bbe
Rasmus Munk <munk1@live.dk>
2019-08-28 17:15:11 +0200

    Mid making tests for the workflowjsoninterface API functions

--- mig/unittest_internal/testworkflowjsoninterface.py
+++ mig/unittest_internal/testworkflowjsoninterface.py
@@ -7,13 +7,17 @@ from shared.workflows import touch_workflow_sessions_db, \
     load_workflow_sessions_db, create_workflow_session_id, \
     delete_workflow_sessions_db, new_workflow_session_id, \
     delete_workflow_session_id
+from shared.functionality.workflowjsoninterface import WORKFLOW_PATTERN,\
+    workflow_api_create, workflow_api_delete
 
 this_path = os.path.dirname(os.path.abspath(__file__))
 
 
-class WorkflowJSONInterfaceTest(unittest.TestCase):
+class WorkflowJSONInterfaceSessionIDTest(unittest.TestCase):
 
     def setUp(self):
+        if not os.environ.get('MIG_CONF', False):
+            os.environ['MIG_CONF'] = '/home/mig/mig/server/MiGserver.conf'
         self.configuration = get_configuration_object()
 
     def tearDown(self):
@@ -66,9 +70,51 @@ class WorkflowJSONInterfaceTest(unittest.TestCase):
         self.assertTrue(delete_workflow_sessions_db(self.configuration))
 
 
-    # def test_create_workflow(self):
-    #     self.assertTrue(True)
-    #
+class WorkflowJSONInterfaceAPIFunctionsTest(unittest.TestCase):
+
+    def setUp(self):
+        self.username = 'FooBar'
+        if not os.environ.get('MIG_CONF', False):
+            os.environ['MIG_CONF'] = '/home/mig/mig/server/MiGserver.conf'
+        self.configuration = get_configuration_object()
+        self.configuration.workflows_db = os.path.join(this_path,
+                                                       'test_sessions_db')
+        touch_workflow_sessions_db(self.configuration, force=True)
+        self.session_id = create_workflow_session_id(self.configuration,
+                                                     self.username)
+        self.assertIsNot(self.session_id, False)
+        self.assertIsNotNone(self.session_id)
+
+    def tearDown(self):
+        if not os.environ.get('MIG_CONF', False):
+            os.environ['MIG_CONF'] = '/home/mig/mig/server/MiGserver.conf'
+        delete_workflow_sessions_db(get_configuration_object())
+
+    def test_create_workflow_pattern(self):
+        pattern_attributes = {'name': 'my new pattern',
+                              'vgrids': 'P-Cubed',
+                              'input_file': 'hdf5_input',
+                              'trigger_paths': ['initial_data/*hdf5'],
+                              'output': {
+                                  'processed_data': 'pattern_0_output/*.hdf5'},
+                              'recipes': ['recipe_0'],
+                              'variables': {'iterations': 20}}
+        print(self.session_id)
+        workflow, msg = workflow_api_create(self.configuration,
+                                            self.session_id,
+                                            WORKFLOW_PATTERN,
+                                            **pattern_attributes)
+        print(workflow, msg)
+        ok, msg = workflow_api_delete(self.configuration,
+                                      self.session_id,
+                                      WORKFLOW_PATTERN,
+                                      **pattern_attributes)
+        print(ok)
+        print(msg)
+
+    # def test_create_workflow_recipe(self):
+    #     workflow = create_workflow(self.configuration, self.username,
+    #                                )
     # def test_read_workflow(self):
     #     self.assertTrue(True)
     #
