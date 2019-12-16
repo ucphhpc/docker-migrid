b4b5cca579dbdab8f35a543a7140abc4a31d265d
PatchOfScotland <d.marchant@ed-alumni.net>
2019-12-16 13:59:51 +0100

    updated job interface to allow for resubmiting and canceling of jobs

--- mig/shared/functionality/jobsjsoninterface.py
+++ mig/shared/functionality/jobsjsoninterface.py
@@ -29,16 +29,22 @@
 
 """JSON interface for job related requests"""
 
+import os
 import sys
 import json
 import shared.returnvalues as returnvalues
+import tempfile
 
-from shared.base import force_utf8_rec
+from shared.base import force_utf8_rec, client_id_dir
+from shared.fileio import unpickle, unpickle_and_change_status, \
+    send_message_to_grid_script
 from shared.init import initialize_main_variables
+from shared.job import new_job
+from shared.mrslkeywords import get_keywords_dict
 from shared.safeinput import REJECT_UNSET, valid_sid, validated_input, \
     html_escape, valid_job_id, valid_job_vgrid, valid_job_attributes, \
     valid_job_type, valid_job_operation
-from shared.job import JOB_TYPES, JOB, QUEUE, get_job_with_id
+from shared.job import JOB_TYPES, JOB, QUEUE, get_job_with_id, fields_to_mrsl
 from shared.workflows import WORKFLOW_TYPES, WORKFLOW_CONSTRUCT_TYPES, \
     WORKFLOW_PATTERN, valid_session_id, get_workflow_with,\
     load_workflow_sessions_db, create_workflow, delete_workflow,\
@@ -120,58 +126,78 @@ JOB_VALUE_MAP = {
 # Job API functions
 def job_api_create(configuration, workflow_session, job_type=JOB,
                    **job_attributes):
-    # """
-    # Handler for 'create' calls to job API.
-    # :param configuration: The MiG configuration object.
-    # :param workflow_session: The MiG job session. This must contain the
-    # key 'owner'
-    # :param workflow_type: [optional] A MiG workflow construct type. This should
-    # be one of 'workflowpattern' or 'workflowrecipe'. Default is
-    # 'workflowpattern'.
-    # :param workflow_attributes: dictionary of arguments used to create the
-    # specified workflow object
-    # :return: (Tuple (boolean, string) or function call to 'create_workflow')
-    # if workflow_type is valid the function 'create_workflow' is called. Else,
-    # a tuple is returned with a first value of False, and an explanatory error
-    # message as the second value.
-    # """
-    # _logger = configuration.logger
-    # _logger.debug("W_API: create: (%s, %s, %s)" % (workflow_session,
-    #                                                workflow_type,
-    #                                                workflow_attributes))
-    #
-    # if workflow_type in WORKFLOW_CONSTRUCT_TYPES:
-    #     return create_workflow(configuration,
-    #                            workflow_session['owner'],
-    #                            workflow_type=workflow_type,
-    #                            **workflow_attributes)
-    # return (False, "Invalid workflow create api type: '%s', valid are: '%s'" %
-    #         (workflow_type,
-    #          ', '.join(WORKFLOW_CONSTRUCT_TYPES)))
-    return (True, 'job_api_create response')
+    """
+    Handler for 'create' calls to job API.
+    :param configuration: The MiG configuration object.
+    :param workflow_session: The MiG job session. This must contain the
+    key 'owner'
+    :param job_type: [optional] A MiG job type. Default is 'job'.
+    :param job_attributes: dictionary of arguments used to create the job
+    :return: Tuple (boolean, string)
+    If a job can be created then a tuple is returned of first value true, and
+    the created job's id in the sceond value. If it cannot be created then a
+    tuple is returned with a first value of False, and an explanatory error
+    message as the second value.
+    """
+
+    client_id = workflow_session['owner']
+    external_dict = get_keywords_dict(configuration)
+    if 'vgrid' in job_attributes:
+        job_attributes.pop('vgrid')
+
+    mrsl = fields_to_mrsl(configuration, job_attributes, external_dict)
+
+    tmpfile = None
+
+    if not configuration.site_enable_jobs:
+        msg = 'Job execution is not enabled on this system'
+        return (False, msg)
+
+    # save to temporary file
+    try:
+        (filehandle, real_path) = tempfile.mkstemp(text=True)
+        os.write(filehandle, mrsl)
+        os.close(filehandle)
+    except Exception, err:
+        msg = 'Failed to write temporary mRSL file: %s' % err
+        configuration.logger.error(msg)
+        return (False, msg)
+
+    # submit it
+    try:
+        (job_status, newmsg, job_id) = \
+            new_job(real_path, client_id, configuration, False, True)
+    except Exception, exc:
+        msg = "Failed to submit new job. Possible invalid mRSL?"
+        configuration.logger.error(msg)
+        return (False, msg)
+
+    if not job_status:
+        configuration.logger.error(newmsg)
+        return (False, newmsg)
+
+    return (True, job_id)
 
 
 def job_api_read(configuration, workflow_session, job_type=JOB,
                  **job_attributes):
-    # """
-    # Handler for 'read' calls to workflow API.
-    # :param configuration: The MiG configuration object.
-    # :param workflow_session: The MiG workflow session. This must contain the
-    # key 'owner'
-    # :param workflow_type: [optional] A MiG workflow read type. This should
-    # be one of 'job', 'queue', 'workflowpattern', 'workflowrecipe', 'any' or
-    # 'pattern_graph'. Default is 'workflowpattern'.
-    # :param workflow_attributes: dictionary of arguments used to select the
-    # workflow object to read.
-    # :return: (Tuple (boolean, string) or function call to 'get_jobs_with',
-    # 'get_workflow_with' or 'search_workflow') If the given workflow_type is
-    # either 'job' or 'queue' the function 'get_jobs_with' will be called. If
-    # the given workflow type is either 'workflowpattern', 'workflowrecipe', or
-    # 'any' the function 'get_workflow_with' is called. If the given
-    # workflow_type is 'pattern_graph' the function 'search_workflow' is called.
-    # If the given workflow_type is none of the above a tuple is returned with a
-    # first value of False, and an explanatory error message as the second value.
-    # """
+    """
+    Handler for 'read' calls to job API.
+    :param configuration: The MiG configuration object.
+    :param workflow_session: The MiG workflow session. This must contain the
+    key 'owner'
+    :param job_type: [optional] A MiG job read type. This should
+    be one of 'job', or 'queue'. Default is 'job'.
+    :param job_attributes: dictionary of arguments used to select the
+    job to read.
+    :return: (Tuple (boolean, string) or function call to 'get_job_with_id')
+    If the given job_type is 'job', the function 'get_job_with_id' will be
+    called. If the given job_type is 'queue' then a tuple is returned with the
+    first value being true and a dictionary of jobs being the second value,
+    with the job ids being the keys. If a problem is encountered a tuple is
+    returned with the first value being false and an explanatory error message
+    for a second value.
+    """
     _logger = configuration.logger
     _logger.debug("J_API: search: (%s, %s, %s)"
                   % (workflow_session, job_type, job_attributes))
@@ -190,7 +216,7 @@ def job_api_read(configuration, workflow_session, job_type=JOB,
         for job in job_list:
             job_dict[job['JOB_ID']] = job
 
-        return  (True, job_dict)
+        return (True, job_dict)
     else:
         if 'job_id' not in job_attributes:
             return (False, "Can't read single job without 'job_id' attribute")
@@ -210,37 +236,74 @@ def job_api_read(configuration, workflow_session, job_type=JOB,
 
 def job_api_update(configuration, workflow_session, job_type=JOB,
                    **job_attributes):
-    # """
-    # Handler for 'update' calls to workflow API.
-    # :param configuration: The MiG configuration object.
-    # :param workflow_session: The MiG workflow session. This must contain the
-    # key 'owner'
-    # :param workflow_type: [optional] A MiG workflow construct type. This should
-    # be one of 'workflowpattern' or 'workflowrecipe'. Default is
-    # 'workflowpattern'.
-    # :param workflow_attributes: dictionary of arguments used to update the
-    # specified workflow object. Must contain key 'vgrid'.
-    # :return: (Tuple (boolean, string) or function call to 'update_workflow')
-    # If the given workflow_type is valid the function 'update_workflow' will be
-    # called. Else, a tuple is returned with a first value of False, and an
-    # explanatory error message as the second value.
-    # """
-    # _logger = configuration.logger
-    # _logger.debug("W_API: update: (%s, %s, %s)" % (workflow_session,
-    #                                                workflow_type,
-    #                                                workflow_attributes))
-    #
-    # if 'vgrid' not in workflow_attributes:
-    #     return (False, "Can't create workflow %s without 'vgrid' attribute"
-    #             % workflow_type)
-    #
-    # if workflow_type in WORKFLOW_CONSTRUCT_TYPES:
-    #     return update_workflow(configuration, workflow_session['owner'],
-    #                            workflow_type, **workflow_attributes)
-    #
-    # return (False, "Invalid workflow update api type: '%s', valid are: '%s'" %
-    #         (workflow_type, ', '.join(WORKFLOW_CONSTRUCT_TYPES)))
-    return (True, 'job_api_update response')
+    """
+    Handler for 'update' calls to job API.
+    :param configuration: The MiG configuration object.
+    :param workflow_session: The MiG workflow session. This must contain the
+    key 'owner'
+    :param job_type: [optional] A MiG job type. Default is 'job'.
+    :param job_attributes: dictionary of arguments used to update the
+    specified workflow object. Currently can only be a job id to cancel.
+    :return: Tuple (boolean, string)
+    If the given job_type is valid the a tuple is returned with true in the
+    first value and a feedback message in the second. Else, a tuple is
+    returned with a first value of False, and an explanatory error message as
+    the second value.
+    """
+
+    _logger = configuration.logger
+
+    _logger.info('GIVEN UPDATE REQUEST WITH ATTRIBUTES: %s' % job_attributes)
+    client_id = workflow_session['owner']
+    job_id = job_attributes['JOB_ID']
+
+    client_dir = client_id_dir(client_id)
+
+    file_path = os.path.join(
+        configuration.mrsl_files_dir, client_dir, job_id + '.mRSL')
+    job = unpickle(file_path, _logger)
+
+    if not job:
+        msg = "Could not open job file for job '%s'" % job_id
+        _logger.error(msg)
+        return (False, msg)
+
+    if 'STATUS' in job_attributes:
+        new_state = 'CANCELED'
+        if job_attributes['STATUS'] == new_state:
+            possible_cancel_states = [
+                'PARSE', 'QUEUED', 'RETRY', 'EXECUTING', 'FROZEN'
+            ]
+
+            if not job['STATUS'] in possible_cancel_states:
+                msg = 'Could not cancel job with status ' + job['STATUS']
+                _logger.error(msg)
+                return (False, msg)
+
+            if not unpickle_and_change_status(file_path, new_state, _logger):
+                _logger.error('%s could not cancel job: %s'
+                              % (client_id, job_id))
+                msg = 'Could not change status of job ' + job_id
+                _logger.error(msg)
+                return (False, msg)
+
+            if not job.has_key('UNIQUE_RESOURCE_NAME'):
+                job['UNIQUE_RESOURCE_NAME'] = 'UNIQUE_RESOURCE_NAME_NOT_FOUND'
+            if not job.has_key('EXE'):
+                job['EXE'] = 'EXE_NAME_NOT_FOUND'
+
+            message = 'JOBACTION ' + job_id + ' ' \
+                      + job['STATUS'] + ' ' + new_state + ' ' \
+                      + job['UNIQUE_RESOURCE_NAME'] + ' ' \
+                      + job['EXE'] + '\n'
+            if not send_message_to_grid_script(message, _logger, configuration):
+                msg = '%s failed to send message to grid script: %s' \
+                      % (client_id, message)
+                _logger.error(msg)
+                return (False, msg)
+            return (True, 'Job %s has been succesfully canceled' % job_id)
+
+    return (False, "No updated applied from attributes '%s'" % job_attributes)
 
 
 def job_api_delete(configuration, workflow_session, job_type=JOB,
