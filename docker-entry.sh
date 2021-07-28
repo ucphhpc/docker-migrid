c2ee3ccc47989e1cc7c656f31f20e5c1398d0ca6
Jonas Bardino <bardino@science.ku.dk>
2021-07-28 09:25:17 +0000

    Use more python packages from yum and add missing required dependency for PyYAML, plus recommended pyOpenSSL and pysendfile used in ftps service. Do not reinstall yum-provided dependency packages with pip. Force wsgidav version 1.3.0 to support python2. Switch to pregenerated 4k dhparams from mozilla instead of generating. Should be reconsidered along with other TLS/SSL choices in case ever used in production. Use same dhparams in nginx to avoid on-demand regenerating there every time on start up. Disable ext_oid since it should be bound to e.g. UCPH openid service to make sense. Tune enable_X options to make sense for test setup and disable some options that don't make sense here. Consitent use of IDMC skin and html snippets. Rework vhost setup for consistent use of custom ports and a single shared plain HTTP vhost with all aliases. Moved shared HTTPS hardening options into a single included conf snippet included everywhere. Adjust settings to Mozilla recommendations. Add workaround to set up local alias if IO daemon address or alias is not available in migrid container. Launch all enabled services with service migrid start rather than explicit daemon start calls.

--- docker-entry.sh
+++ docker-entry.sh
@@ -3,6 +3,25 @@
 # Launches the MiG web interface and the underlying services
 # Continuously checks for whether the services are still alive
 
+# Lookup DOMAIN dynamically from MiGserver.conf
+DOMAIN="$(egrep '^server_fqdn' $MIG_ROOT/mig/server/MiGserver.conf | cut -d ' ' -f 3)"
+
+# Map external addresses to local IPs for daemons to bind to if it isn't
+# already exposed with docker-compose network alias.
+DAEMONFQDN="io.${DOMAIN}"
+DAEMONIP=$(getent hosts ${DAEMONFQDN}|cut -d ' ' -f 1)
+FOUNDIP=0
+for LOCALIP in $(ifconfig | grep inet | grep -v '127.0.0.' | cut -d ' ' -f 10); do
+    if [ ${LOCALIP} = ${DAEMONIP} ]; then
+        echo "Found existing local IP for daemons"
+        FOUNDIP=1
+    fi
+done
+if [ ${FOUNDIP} -eq 0 ]; then
+    echo "Add local IP alias for daemons"
+    echo "${LOCALIP}	${DAEMONFQDN}" >> /etc/hosts
+fi
+echo "Binding daemons to IP: $(getent hosts ${DAEMONFQDN}|cut -d ' ' -f 1)"
 
 while getopts u:p: option
 do
@@ -30,6 +49,8 @@ fi
 # Load required httpd environment vars
 source migrid-httpd.env
 
+# TODO: can we switch to proper service start?
+#service httpd start
 /usr/sbin/httpd -k start
 status=$?
 if [ $status -ne 0 ]; then
@@ -38,35 +59,19 @@ if [ $status -ne 0 ]; then
 fi
 
 # Start every service for now
-# TODO make individual checks for each service
-service migrid startdaemon openid
-ps aux | grep openid | grep -q -v grep
+service migrid start
 status=$?
 if [ $status -ne 0 ]; then
-    echo "Failed to start openid: $status"
+    echo "Failed to start migrid services: $status"
     exit $status
 fi
 
-service migrid startdaemon sftpsubsys
-ps aux | grep sshd | grep -q -v grep
-status=$?
-if [ $status -ne 0 ]; then
-    echo "Failed to start sshd: $status"
-    exit $status
-fi
-
+# Check core services
 while sleep 60; do
     ps aux | grep openid | grep -q -v grep
     OPENID_STATUS=$?
     if [ $OPENID_STATUS -ne 0 ]; then
         echo "OpenID service failed."
-        exit 1
-    fi
-
-    ps aux | grep sshd | grep -q -v grep
-    SFTP_STATUS=$?
-    if [ $SFTP_STATUS -ne 0 ]; then
-        echo "sshd service failed."
         exit 1
     fi
 
