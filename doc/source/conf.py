9a1ec8729e178b55d1b1beece0b9e078126b5784
Jonas Bardino <bardino@nbi.ku.dk>
2024-05-21 18:07:12 +0200

    Minor: update copyright year.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -4,7 +4,7 @@ import sphinx_rtd_theme
 # -- Project information -----------------------------------------------------
 
 project = 'docker-migrid'
-copyright = '2023, Jonas Bardino, Martin Rehr, Rasmus Munk and Benedikt Alexander Braunger'
+copyright = '2024, Jonas Bardino, Martin Rehr, Rasmus Munk and Benedikt Alexander Braunger'
 author = 'Jonas Bardino, Martin Rehr, Rasmus Munk, Benedikt Alexander Braunger'
 
 
