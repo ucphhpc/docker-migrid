87deaf9ac88c51c1731fb7ca79e1a82c7482205a
Jonas Bardino <bardino@science.ku.dk>
2025-10-14 14:07:46 +0200

    Initial support for hotfix integration as outlined in milestone 6.
    Adds a new `/hotfixes` folder in containers and `${PERSISTENT_ROOT}/hotfixes`
    with `scripts` and `patches` in it on the host.
    Adds a simple script to help apply hotfixes from a folder and installs it in
    `/app/apply-hotfixes.sh` in all containers.
    All containers mount `${PERSISTENT_ROOT}/hotfixes` on `/hotfixes` and
    `docker-entry.sh` is extended to first run `/app/apply-hotfixes.sh`.
    It traverses `/hotfixes` and sequentially applies any patches found in
    `/hotfix/patches` and runs any scripts found in `/hotfix/scripts` before
    proceeding with the usual container execution to effectively hotfix everything
    inside the container before running.

---
+++ apply-hotfixes.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# Apply all hot-fixes in specified folder
+
+APPLIED_DIR="/tmp/hotfixes-applied"
+HOTFIXES_DIR="/hotfixes"
+if [ $# -gt 0 ]; then
+    HOTFIXES_DIR="$1"
+fi
+PATCH_SOURCE="${HOTFIXES_DIR}/patches"
+SCRIPT_SOURCE="${HOTFIXES_DIR}/scripts"
+PATCHES_APPLIED="${APPLIED_DIR}/patches"
+SCRIPTS_APPLIED="${APPLIED_DIR}/scripts"
+
+if [ -d "${HOTFIXES_DIR}" ]; then
+    echo "DEBUG: Applying hot-fixes available in ${HOTFIXES_DIR}"
+    mkdir -p ${PATCHES_APPLIED} ${SCRIPTS_APPLIED}
+    if [ -d "${PATCH_SOURCE}" ]; then
+        echo "DEBUG: Applying any patches available in ${PATCH_SOURCE}"
+        for PATCH_PATH in "${PATCH_SOURCE}"/* ; do
+            PATCH_NAME=$(basename "${PATCH_PATH}")
+            if [ ! -f "${PATCH_PATH}" ]; then
+                # skip anything but files
+                continue
+            fi
+            if [ -f "${PATCHES_APPLIED}/${PATCH_NAME}" ]; then
+                echo "DEBUG: skip already applied patch: ${PATCH_NAME}"
+            else
+                echo "Applying patch ${PATCH_PATH}"
+                patch -p0 < "${PATCH_PATH}" && \
+                    cp "${PATCH_PATH}" "${PATCHES_APPLIED}/"
+            fi
+        done
+    fi
+    if [ -d "${SCRIPT_SOURCE}" ]; then
+        echo "Applying any scripts available in ${SCRIPT_SOURCE}"
+        for SCRIPT_PATH in "${SCRIPT_SOURCE}"/* ; do
+            SCRIPT_NAME=$(basename "${SCRIPT_PATH}")
+            if [ ! -f "${SCRIPT_PATH}" ]; then
+                # skip anything but files
+                continue
+            fi
+            if [ -f "${SCRIPTS_APPLIED}/${SCRIPT_NAME}" ]; then
+                echo "DEBUG: skip already applied script: ${SCRIPT_NAME}"
+            else
+                echo "Running script ${SCRIPT_PATH}"
+                ${SCRIPT_PATH} && \
+                    cp "${SCRIPT_PATH}" "${SCRIPTS_APPLIED}/"
+            fi
+        done
+    fi
+    echo "DEBUG: Applied hot-fixes available in ${HOTFIXES_DIR}"
+else
+    echo "WARNING: no such hot-fixes folder ${HOTFIXES_DIR}"
+    exit 1
+fi
+exit 0
+
