1e902d08b4a440ef0d4b6a0e5741226faa0fae17
Jonas Bardino <bardino@science.ku.dk>
2025-10-14 16:25:52 +0200

    Silence most debug output to wrap up this first fully functional version.

--- apply-hotfixes.sh
+++ apply-hotfixes.sh
@@ -13,10 +13,10 @@ PATCHES_APPLIED="${APPLIED_DIR}/patches"
 SCRIPTS_APPLIED="${APPLIED_DIR}/scripts"
 
 if [ -d "${HOTFIXES_DIR}" ]; then
-    echo "DEBUG: Applying hot-fixes available in ${HOTFIXES_DIR}"
+    #echo "DEBUG: Applying hot-fixes available in ${HOTFIXES_DIR}"
     mkdir -p ${PATCHES_APPLIED} ${SCRIPTS_APPLIED}
     if [ -d "${PATCH_SOURCE}" ]; then
-        echo "DEBUG: Applying any patches available in ${PATCH_SOURCE}"
+        #echo "DEBUG: Applying any patches available in ${PATCH_SOURCE}"
         for PATCH_PATH in "${PATCH_SOURCE}"/* ; do
             PATCH_NAME=$(basename "${PATCH_PATH}")
             if [ ! -f "${PATCH_PATH}" ]; then
@@ -24,16 +24,16 @@ if [ -d "${HOTFIXES_DIR}" ]; then
                 continue
             fi
             if [ -f "${PATCHES_APPLIED}/${PATCH_NAME}" ]; then
-                echo "DEBUG: skip already applied patch: ${PATCH_NAME}"
+                echo "Skip already applied patch: ${PATCH_NAME}"
             else
-                echo "Applying patch ${PATCH_PATH}"
+                #echo "DEBUG: applying patch ${PATCH_PATH}"
                 patch -d / -p0 < "${PATCH_PATH}" && \
                     cp "${PATCH_PATH}" "${PATCHES_APPLIED}/"
             fi
         done
     fi
     if [ -d "${SCRIPT_SOURCE}" ]; then
-        echo "Applying any scripts available in ${SCRIPT_SOURCE}"
+        #echo "DEBUG: Applying any scripts available in ${SCRIPT_SOURCE}"
         for SCRIPT_PATH in "${SCRIPT_SOURCE}"/* ; do
             SCRIPT_NAME=$(basename "${SCRIPT_PATH}")
             if [ ! -f "${SCRIPT_PATH}" ]; then
@@ -41,18 +41,18 @@ if [ -d "${HOTFIXES_DIR}" ]; then
                 continue
             fi
             if [ -f "${SCRIPTS_APPLIED}/${SCRIPT_NAME}" ]; then
-                echo "DEBUG: skip already applied script: ${SCRIPT_NAME}"
+                echo "Skip already applied script: ${SCRIPT_NAME}"
             else
-                echo "Running script ${SCRIPT_PATH}"
+                #echo "DEBUG: running script ${SCRIPT_PATH}"
                 ${SCRIPT_PATH} && \
                     cp "${SCRIPT_PATH}" "${SCRIPTS_APPLIED}/"
             fi
         done
     fi
-    echo "DEBUG: Applied hot-fixes available in ${HOTFIXES_DIR}"
+    #echo "DEBUG: Applied hot-fixes available in ${HOTFIXES_DIR}"
+    exit 0
 else
     echo "WARNING: no such hot-fixes folder ${HOTFIXES_DIR}"
     exit 1
 fi
-exit 0
 
