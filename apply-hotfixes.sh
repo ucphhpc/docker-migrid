21386dbd517c07870da386e81831cb3f60e0c4d4
Martin Rehr <rehr@nbi.ku.dk>
2025-11-19 11:40:45 +0100

    Log messages and hotfix output for each container to '/var/log/migrid-container-hotfixes.log'

--- apply-hotfixes.sh
+++ apply-hotfixes.sh
@@ -11,9 +11,12 @@ PATCH_SOURCE="${HOTFIXES_DIR}/patches"
 SCRIPT_SOURCE="${HOTFIXES_DIR}/scripts"
 PATCHES_APPLIED="${APPLIED_DIR}/patches"
 SCRIPTS_APPLIED="${APPLIED_DIR}/scripts"
+LOGFILE="/var/log/migrid-container-hotfixes.log"
 
+touch ${LOGFILE}
 if [ -d "${HOTFIXES_DIR}" ]; then
-    #echo "DEBUG: Applying hot-fixes available in ${HOTFIXES_DIR}"
+    echo "$(date) $(hostname): Applying hot-fixes available in ${HOTFIXES_DIR}" \
+        >> ${LOGFILE}
     mkdir -p ${PATCHES_APPLIED} ${SCRIPTS_APPLIED}
     if [ -d "${PATCH_SOURCE}" ]; then
         #echo "DEBUG: Applying any patches available in ${PATCH_SOURCE}"
@@ -24,11 +27,17 @@ if [ -d "${HOTFIXES_DIR}" ]; then
                 continue
             fi
             if [ -f "${PATCHES_APPLIED}/${PATCH_NAME}" ]; then
-                echo "Skip already applied patch: ${PATCH_NAME}"
+                echo "$(date) $(hostname): Skip already applied patch: ${PATCH_NAME}" \
+                    >> ${LOGFILE}
             else
-                #echo "DEBUG: applying patch ${PATCH_PATH}"
-                patch -d / -p0 < "${PATCH_PATH}" && \
-                    cp "${PATCH_PATH}" "${PATCHES_APPLIED}/"
+                echo "$(date) $(hostname): Applying patch ${PATCH_PATH}" \
+                    >> ${LOGFILE} 2>&1
+                patch -d / -p0 < "${PATCH_PATH}" >> ${LOGFILE} 2>&1
+                ret=$?
+                echo "$(date) $(hostname): Applied patch: ${PATCH_PATH}: $ret" \
+                    >> ${LOGFILE}
+                [ "$ret" -eq 0 ] \
+                    && cp "${PATCH_PATH}" "${PATCHES_APPLIED}/" >> ${LOGFILE} 2>&1
             fi
         done
     fi
@@ -36,23 +45,27 @@ if [ -d "${HOTFIXES_DIR}" ]; then
         #echo "DEBUG: Applying any scripts available in ${SCRIPT_SOURCE}"
         for SCRIPT_PATH in "${SCRIPT_SOURCE}"/* ; do
             SCRIPT_NAME=$(basename "${SCRIPT_PATH}")
-            if [ ! -f "${SCRIPT_PATH}" ]; then
-                # skip anything but files
+            if [[ ! -f "${SCRIPT_PATH}" && ! -x "${SCRIPT_PATH}" ]]; then
+                # skip anything but executable files
                 continue
             fi
             if [ -f "${SCRIPTS_APPLIED}/${SCRIPT_NAME}" ]; then
-                echo "Skip already applied script: ${SCRIPT_NAME}"
+                echo "$(date) $(hostname): Skip already applied script: ${SCRIPT_NAME}" \
+                    >> ${LOGFILE}
             else
-                #echo "DEBUG: running script ${SCRIPT_PATH}"
-                ${SCRIPT_PATH} && \
-                    cp "${SCRIPT_PATH}" "${SCRIPTS_APPLIED}/"
+                echo "$(date) $(hostname): Running script ${SCRIPT_PATH}" >> ${LOGFILE}
+                ${SCRIPT_PATH} >> ${LOGFILE} 2>&1 
+                ret=$?
+                echo "$(date) $(hostname): Finished script ${SCRIPT_PATH}: $ret" >> ${LOGFILE}
+                [ "$ret" -eq 0 ] \
+                    && cp "${SCRIPT_PATH}" "${SCRIPTS_APPLIED}/"
             fi
         done
     fi
     #echo "DEBUG: Applied hot-fixes available in ${HOTFIXES_DIR}"
     exit 0
 else
-    echo "WARNING: no such hot-fixes folder ${HOTFIXES_DIR}"
+    echo "WARNING: no such hot-fixes folder ${HOTFIXES_DIR}" >> ${LOGFILE}
     exit 1
 fi
 
